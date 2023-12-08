0888d044aa1b86ea41036b42151243886a12ff74
Saurabh Lohiya <saurabhlohiya1234@gmail.com>
2023-12-08 10:43:06 +0530

    Shifted to express from bun

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-import { userRouter } from "./routes/users";
-import { taskRouter } from "./routes/tasks";
-
-import express, { Request, Response, Application } from "express";
+import userRouter from "./routes/users";
+import taskRouter from "./routes/tasks";
+import express, { Request, Response, Application, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 const server: Application = express();
@@ -10,12 +9,17 @@ const prisma = new PrismaClient();
 const SECRET_KEY = String(process.env.SECRET_KEY);
 
 // Middleware to authenticate requests
-export async function authMiddleware(req: Request, res: Response, next: any) {
+export async function authMiddleware(
+	req: Request,
+	res: Response,
+	next: NextFunction
+): Promise<void> {
 	try {
 		const token = req.headers?.authorization;
 
 		if (!token) {
-			return res.status(401).json({ error: "Unauthorized" });
+			res.status(401).json({ error: "Unauthorized" });
+			throw new Error("Unauthorized");
 		}
 		const decoded: any = jwt.verify(token, SECRET_KEY);
 		let user = await prisma.user.findUnique({
@@ -29,8 +33,8 @@ export async function authMiddleware(req: Request, res: Response, next: any) {
 	}
 }
 
-server.use(taskRouter);
-server.use(userRouter);
+server.use("/tasks", authMiddleware, taskRouter);
+server.use("/users", userRouter);
 
 server.listen(Number(process.env.PORT), () => {
 	console.log(`App is listening on port ${process.env.PORT}`);
